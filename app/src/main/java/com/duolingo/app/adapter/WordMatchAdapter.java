aa39ee348b7d10f38b454e48ce81f793abe3ff4d
Xavi-PL <xavipl.mvk25@gmail.com>
2021-03-14 20:23:41 +0100

    ADDED: Nuevo LAYOUT con RecyclerView - WordMatchActivity

---
+++ app/src/main/java/com/duolingo/app/adapter/WordMatchAdapter.java
@@ -0,0 +1,83 @@
+package com.duolingo.app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.duolingo.app.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordMatchAdapter extends RecyclerView.Adapter<com.duolingo.app.adapter.WordMatchAdapter.ViewHolder>{
+
+    private WordMatchAdapter.OnNoteListener mOnNoteListener;
+    private ArrayList<String> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public WordMatchAdapter(ArrayList<String> itemList, Context context, WordMatchAdapter.OnNoteListener onNoteListener){
+        this.mOnNoteListener = onNoteListener;
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.mData = itemList;
+    }
+
+    public WordMatchAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.item_word_match, null);
+        return new WordMatchAdapter.ViewHolder(view, mOnNoteListener);
+
+    }
+
+    public void setItems(ArrayList<String> items) {
+        mData = items;
+    }
+
+    public void onBindViewHolder(final WordMatchAdapter.ViewHolder holder, final int position){
+        holder.bindData(mData.get(position), position);
+    }
+
+    public interface OnNoteListener{
+        void onNoteClick(int position);
+    }
+
+    public int getItemCount(){
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView tvWordToMatch;
+        EditText etWordToMatch;
+        CardView cvResult;
+        WordMatchAdapter.OnNoteListener onNoteListener;
+
+        ViewHolder(View itemView, WordMatchAdapter.OnNoteListener onNoteListener){
+            super(itemView);
+            tvWordToMatch = itemView.findViewById(R.id.rTvWordToMatch);
+            etWordToMatch = itemView.findViewById(R.id.rEtWordToMatch);
+            cvResult = itemView.findViewById(R.id.rCvResult);
+            this.onNoteListener = onNoteListener;
+            itemView.setOnClickListener(this);
+        }
+
+        void bindData(String item, int position){
+            tvWordToMatch.setText(item);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onNoteListener.onNoteClick(getAdapterPosition());
+
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/duolingo/app/tasks/WordMatchActivity.java
+++ app/src/main/java/com/duolingo/app/tasks/WordMatchActivity.java
@@ -1,30 +1,31 @@
 package com.duolingo.app.tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.duolingo.app.R;
+import com.duolingo.app.adapter.RankingAdapter;
+import com.duolingo.app.adapter.SpacesItemDecoration;
+import com.duolingo.app.adapter.WordMatchAdapter;
 import com.duolingo.app.model.Exercice;
 import com.duolingo.app.util.ExerciceActivity;
-import com.nex3z.flowlayout.FlowLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
-public class WordMatchActivity extends AppCompatActivity {
+public class WordMatchActivity extends AppCompatActivity implements WordMatchAdapter.OnNoteListener{
 
     private HashMap<String, String> wordPairs = new HashMap<>();
     private ArrayList<String> arrayWords = new ArrayList<>();
-    private ArrayList<String> arrayPairs = new ArrayList<>();
-    private int totalPairs = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +34,12 @@ public class WordMatchActivity extends AppCompatActivity {
 
         getData();
 
-        Collections.shuffle(arrayWords);
-        FlowLayout flWordsPairs = findViewById(R.id.flWordsPairs);
-        for (String s : arrayWords) {
-            Button button = new Button(this);
-            button.setText(s);
-            button.setBackgroundResource(R.drawable.next_layout);
-            flWordsPairs.addView(button);
-        }
-
-        Collections.shuffle(arrayPairs);
-        FlowLayout flMatchsPairs = findViewById(R.id.flMatchsPairs);
-        for (String s : arrayPairs) {
-            Button button = new Button(this);
-            button.setText(s);
-            button.setBackgroundResource(R.drawable.next_layout);
-            flMatchsPairs.addView(button);
-        }
+        RecyclerView rvWordMatches = findViewById(R.id.rvWordMatches);
+        WordMatchAdapter listAdapter = new WordMatchAdapter(arrayWords, getApplicationContext(), this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        rvWordMatches.addItemDecoration(new SpacesItemDecoration(15));
+        rvWordMatches.setLayoutManager(layoutManager);
+        rvWordMatches.setAdapter(listAdapter);
 
     }
 
@@ -70,14 +60,17 @@ public class WordMatchActivity extends AppCompatActivity {
                 for (int i = 1; i < 100; i++){
                     JSONObject rawPair = rawData.getJSONObject("wordMatch"+i);
                     arrayWords.add((String) rawPair.get("match"));
-                    arrayPairs.add((String) rawPair.get("word"));
                     wordPairs.put( (String) rawPair.get("match"), (String) rawPair.get("word"));
-                    totalPairs++;
                 }
                 
             } catch (JSONException e) {
                 System.out.println("[DEBUG] - Todas las parejas obtenidas!");
             }
         }
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+
     }
 }
